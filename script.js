d28bd7c4a261337a1c5685a0115517de82988850
tucelek@gmail.com <tucelek@gmail.com>
2020-08-04 19:48:37 +0200

    clean up

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const burger = document.querySelector('.plus');
-const i = document.querySelector('i');
 
 burger.addEventListener('click', function(){
   burger.classList.toggle('active')
-})
-
-i.addEventListener('click', function(){
-  i.classList.toggle('active')
 })
